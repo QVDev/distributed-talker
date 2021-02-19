a6b0d951b0f02ff628b35403662c15548ba6e744
QVDev <feedbackandroid@gmail.com>
2021-02-19 18:40:47 +0100

    Style audio visualizer

--- visualizer.js
+++ visualizer.js
@@ -1,4 +1,10 @@
+var isStarted = false;
+
 function init() {
+    if (isStarted == true) {
+        return;
+    }
+    isStarted = true;
     // Older browsers might not implement mediaDevices at all, so we set an empty object first
     if (navigator.mediaDevices === undefined) {
         navigator.mediaDevices = {};
@@ -33,9 +39,7 @@ function init() {
     // window. is needed otherwise Safari explodes
 
     var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-    var voiceSelect = document.getElementById("voice");
     var source;
-    var stream;
 
     // grab the mute button to use below
 
@@ -48,60 +52,14 @@ function init() {
     analyser.maxDecibels = -10;
     analyser.smoothingTimeConstant = 0.85;
 
-    var distortion = audioCtx.createWaveShaper();
     var gainNode = audioCtx.createGain();
-    var biquadFilter = audioCtx.createBiquadFilter();
-    var convolver = audioCtx.createConvolver();
-
-    // distortion curve for the waveshaper, thanks to Kevin Ennis
-    // http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion
-
-    function makeDistortionCurve(amount) {
-        var k = typeof amount === 'number' ? amount : 50,
-            n_samples = 44100,
-            curve = new Float32Array(n_samples),
-            deg = Math.PI / 180,
-            i = 0,
-            x;
-        for (; i < n_samples; ++i) {
-            x = i * 2 / n_samples - 1;
-            curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
-        }
-        return curve;
-    };
-
-    // grab audio track via XHR for convolver node
-
-    var soundSource;
-
-    ajaxRequest = new XMLHttpRequest();
-
-    ajaxRequest.open('GET', 'https://mdn.github.io/voice-change-o-matic/audio/concert-crowd.ogg', true);
-
-    ajaxRequest.responseType = 'arraybuffer';
-
-
-    ajaxRequest.onload = function() {
-        var audioData = ajaxRequest.response;
-
-        audioCtx.decodeAudioData(audioData, function(buffer) {
-            soundSource = audioCtx.createBufferSource();
-            convolver.buffer = buffer;
-        }, function(e) { console.log("Error with decoding audio data" + e.err); });
-
-        //soundSource.connect(audioCtx.destination);
-        //soundSource.loop = true;
-        //soundSource.start();
-    };
-
-    ajaxRequest.send();
 
     // set up canvas context for visualizer
 
     var canvas = document.querySelector('.visualizer');
     var canvasCtx = canvas.getContext("2d");
 
-    var intendedWidth = document.querySelector('.wrapper').clientWidth;
+    var intendedWidth = document.getElementById('room-grid').clientWidth;
 
     canvas.setAttribute('width', intendedWidth);
 
@@ -118,15 +76,11 @@ function init() {
             .then(
                 function(stream) {
                     source = audioCtx.createMediaStreamSource(stream);
-                    source.connect(distortion);
-                    distortion.connect(biquadFilter);
-                    biquadFilter.connect(gainNode);
-                    convolver.connect(gainNode);
+                    source.connect(gainNode);
                     gainNode.connect(analyser);
                     analyser.connect(audioCtx.destination);
 
                     visualize();
-                    voiceChange();
                 })
             .catch(function(err) { console.log('The following gUM error occured: ' + err); })
     } else {
@@ -226,43 +180,11 @@ function init() {
 
     }
 
-    function voiceChange() {
-
-        distortion.oversample = '4x';
-        biquadFilter.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
-
-        var voiceSetting = voiceSelect.value;
-        console.log(voiceSetting);
-
-        //when convolver is selected it is connected back into the audio path
-        if (voiceSetting == "convolver") {
-            biquadFilter.disconnect(0);
-            biquadFilter.connect(convolver);
-        } else {
-            biquadFilter.disconnect(0);
-            biquadFilter.connect(gainNode);
-
-            if (voiceSetting == "distortion") {
-                distortion.curve = makeDistortionCurve(400);
-            } else if (voiceSetting == "biquad") {
-                biquadFilter.type = "lowshelf";
-                biquadFilter.frequency.setTargetAtTime(1000, audioCtx.currentTime, 0)
-                biquadFilter.gain.setTargetAtTime(25, audioCtx.currentTime, 0)
-            } else if (voiceSetting == "off") {
-                console.log("Voice settings turned off");
-            }
-        }
-    }
-
     // event listeners to change visualize and voice settings
 
     visualSelect.onchange = function() {
         window.cancelAnimationFrame(drawVisual);
         visualize();
-    };
-
-    voiceSelect.onchange = function() {
-        voiceChange();
     };
 
     mute.onclick = voiceMute;
