ed9eccd9a8a1a20020571c1e5e8da05d6556ae6c
QVDev <feedbackandroid@gmail.com>
2021-02-22 14:17:57 +0100

    Try this with websocket

---
+++ public/js/websocket.js
@@ -0,0 +1,64 @@
+var webSocket = new WebSocket("wss://gunptt.herokuapp.com/gun");
+var sink;
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+var uuid = uuidv4();
+
+webSocket.onopen = function(event) {
+    console.log(event);
+};
+
+webSocket.onmessage = function(event) {
+    const msg = JSON.parse(event.data);
+    if (!msg.action || msg.uuid == uuid) {
+        return;
+    }
+    console.log(msg);
+    switch (msg.action) {
+        case "join":
+            joiner(msg.roomId);
+            break;
+        case "leave":
+            leaver(msg.roomId);
+            break;
+        case "create":
+            addRoom(msg.roomId, msg.roomDesc);
+            break;
+        case "audio":
+            if (msg.roomId !== window.room || window.room == undefined) {
+                addRoom(msg.roomId, msg.roomDesc);
+                return;
+            }
+
+            if (window.launchedContext == undefined || window.launchedContext == false) {
+                return;
+            }
+            if (sink == undefined) {
+                sink = new XAudioServer(1, 8000, 320, 512, function(samplesRequested) {}, 0);
+            }
+
+            let buffer = new Float32Array(Object.values(msg.data));
+            sink.writeAudio(buffer);
+            break;
+    }
+}
+
+function send(data, action) {
+    if (window.room == undefined) {
+        return;
+    }
+
+    webSocket.send(JSON.stringify({
+        roomId: window.room,
+        roomDesc: window.desc,
+        data: data,
+        action: action,
+        uuid: uuid
+    }));
+}
