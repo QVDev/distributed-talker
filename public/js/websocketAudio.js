0e83bf1015dbe0571d2c15f075a4a074ed32cd21
QVDev <feedbackandroid@gmail.com>
2021-03-17 18:25:54 +0100

    Try with jitter buffer example based on latency

--- public/js/websocketAudio.js
+++ public/js/websocketAudio.js
@@ -6,6 +6,8 @@ function startReceiveAudio(room) {
     dataSocket.binaryType = "arraybuffer";
     // var sink;
     var player;
+    var lastTime = 0;
+    var jitterBuffer = new Map()
 
     dataSocket.onopen = function(event) {
         // console.log(event);
@@ -13,6 +15,16 @@ function startReceiveAudio(room) {
 
     dataSocket.onmessage = function(msg) {
         let sender = new Uint8Array(msg.data, 0, 3);
+        let time = new Uint8Array(msg.data, 3, 13);
+        time = time.toString().replaceAll(',', '')
+
+
+        let diff = time - lastTime
+        let latency = Date.now() - time;
+        // console.log("latency::" + latency);
+        lastTime = time;
+
+        // console.log(time);
         if (sender[0] == uuid.toString().charAt(0) && sender[1] == uuid.toString().charAt(1) && sender[2] == uuid.toString().charAt(2)) {
             return
         }
@@ -26,21 +38,51 @@ function startReceiveAudio(room) {
                 encoding: '32bitFloat',
                 channels: CHANNELS,
                 sampleRate: TO_SAMPLE_RATE,
-                flushingTime: 0.08
+                flushingTime: 1.00
             });
         }
         // if (sink == undefined) {
         //     sink = new XAudioServer(CHANNELS, TO_SAMPLE_RATE, MIN_BUFFER_SIZE, MAX_BUFFER_SIZE, function(samplesRequested) {}, 0);
         // }
 
-        let buffer = new Uint8Array(msg.data, 3, msg.data.length);
-        decoded = codec.decode(buffer);
+        let buffer = new Uint8Array(msg.data, 16, msg.data.length);
+        jitterBuffer.set(time, buffer);
+
+        if (jitterBuffer.size > latency) {
+            var mapAsc = new Map([...jitterBuffer.entries()].sort());
+            jitterBuffer.clear();
+            mapAsc.forEach(value => {
+                decoded = codec.decode(value);
+                player.feed(decoded);
+            });
+        }
 
         // sink.writeAudio(decoded);
-        player.feed(decoded);
+
         buffer = null;
     }
 }
+
+function compareMaps(map1, map2) {
+    var testVal;
+    if (map1.size !== map2.size) {
+        return false;
+    }
+    for (var [key, val] of map1) {
+        testVal = map2.get(key);
+        // in cases of an undefined value, make sure the key
+        // actually exists on the object so there are no false positives
+        if (JSON.stringify(testVal) !== JSON.stringify(val) || (testVal === undefined && !map2.has(key))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function sortNumber(a, b) {
+    return a - b;
+}
+
 
 function stopReceiveAudio() {
     dataSocket.close();
@@ -53,7 +95,9 @@ function sendAudio(data) {
         return;
     }
     var id = new Uint8Array([uuid.toString().charAt(0), uuid.toString().charAt(1), uuid.toString().charAt(2)]);
-    var concatArray = concat(id, data[0])
+    var time = Uint8Array.from(Date.now().toString());
+    var prefix = concat(id, time);
+    var concatArray = concat(prefix, data[0])
     dataSocket.send(concatArray);
 }
 
